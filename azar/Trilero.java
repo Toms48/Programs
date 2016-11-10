80b798e3095da6203e8294562e321b4b596bf11b
jolsensei <PasioNintendo@gmail.com>
2016-11-10 12:49:23 +0100

    Trile fnished

--- azar/Trilero.java
+++ azar/Trilero.java
@@ -35,13 +35,13 @@ public class Trilero {
 		char respuesta = ' ';
 		
 		//Preguntar si quieres jugar y validar
-			System.out.print(" ¿Quiere jugar? Soy de fiar, no le engañaré (s/n) " );
+			System.out.print(" ¿Quiere jugar? Soy de fiar, no le engañare (s/n) " );
 				respuesta = Character.toLowerCase(teclado.next( ).charAt(0) );
 				System.out.println(" ");
 				
 			while(respuesta != 's' && respuesta != 'n'){
 					
-			System.out.print(" ¿Quiere jugar? Soy de fiar, no le engañaré (s/n) " );
+			System.out.print(" ¿Quiere jugar? Soy de fiar, no le engañare (s/n) " );
 				respuesta = Character.toLowerCase(teclado.next( ).charAt(0) );	
 				System.out.println(" ");
 			}
@@ -56,49 +56,175 @@ public class Trilero {
 		
 		//Escoger cubilete y mostrar resultado	
 			switch(aleatorio){
-			
 				case 0:
-					System.out.println("La bola estara en la izquierda");
+					//System.out.println("La bola estara en la izquierda");
 					System.out.println("        ______             ______             ______        ");
-					System.out.println("       #      #           #      #           #      #          ");
-					System.out.println("      #        #         #        #         #        #         ");
-					System.out.println("     #          #       #          #       #          #        ");
-					System.out.println("    #            #     #            #     #            #       ");
-					System.out.println("   #              #   #              #   #              #      ");
-					System.out.println("  #                # #                # #                #     ");
+					System.out.println("       #      #           #      #           #      #       ");
+					System.out.println("      #        #         #        #         #        #      ");
+					System.out.println("     #          #       #          #       #          #     ");
+					System.out.println("    #      i     #     #      c     #     #     d      #    ");
+					System.out.println("   #              #   #              #   #              #   ");
+					System.out.println("  #                # #                # #                #  ");
 					System.out.println(" ");
+						do{
 						System.out.println("Escoja cubilete");
 						System.out.println(" ");
 						System.out.println("i----> Izquierda");
 						System.out.println("c----> Centro");
 						System.out.println("d----> Derecha");
-						
+							respuesta = Character.toLowerCase(teclado.next( ).charAt(0) );
+							}while(respuesta != 'i' && respuesta != 'c' && respuesta != 'd');
+										switch(respuesta){
+											
+										case 'i':
+											System.out.println("                           ______             ______        ");
+											System.out.println("                          #      #           #      #       ");
+											System.out.println("                         #        #         #        #      ");
+											System.out.println("       ###              #          #       #          #     ");
+											System.out.println("      #####            #      c     #     #     d      #    ");
+											System.out.println("      #####           #              #   #              #   ");
+											System.out.println("       ###           #                # #                #  ");
+												System.out.println("ENHORABUENA, HAS GANADO");
+										break;
+										
+										
+										case 'c':
+											System.out.println("        ______                                ______        ");
+											System.out.println("       #      #                              #      #       ");
+											System.out.println("      #        #                            #        #      ");
+											System.out.println("     #          #                          #          #     ");
+											System.out.println("    #      i     #                        #     d      #    ");
+											System.out.println("   #              #                      #              #   ");
+											System.out.println("  #                #                    #                #  ");
+												System.out.println("HAS PERDIDO, SE SIENTE");
+										break;
+										
+										case 'd':
+											System.out.println("        ______             ______       ");
+											System.out.println("       #      #           #      #      ");
+											System.out.println("      #        #         #        #     ");
+											System.out.println("     #          #       #          #    ");
+											System.out.println("    #      i     #     #      c     #   ");
+											System.out.println("   #              #   #              #  ");
+											System.out.println("  #                # #                # ");
+												System.out.println("HAS PERDIDO, SE SIENTE");
+										break;
+										}//Fin_switch			
 				break;
 				
 				case 1:
-					System.out.println("La bola estara en el centro");
+					//System.out.println("La bola estara en el centro");
 					System.out.println("        ______             ______             ______        ");
-					System.out.println("       #      #           #      #           #      #          ");
-					System.out.println("      #        #         #        #         #        #         ");
-					System.out.println("     #          #       #          #       #          #        ");
-					System.out.println("    #            #     #            #     #            #       ");
-					System.out.println("   #              #   #              #   #              #      ");
-					System.out.println("  #                # #                # #                #     ");
+					System.out.println("       #      #           #      #           #      #       ");
+					System.out.println("      #        #         #        #         #        #      ");
+					System.out.println("     #          #       #          #       #          #     ");
+					System.out.println("    #      i     #     #      c     #     #     d      #    ");
+					System.out.println("   #              #   #              #   #              #   ");
+					System.out.println("  #                # #                # #                #  ");
 					System.out.println(" ");
+					System.out.println("Escoja cubilete");
+						do{
+						System.out.println(" ");
+						System.out.println("i----> Izquierda");
+						System.out.println("c----> Centro");
+						System.out.println("d----> Derecha");
+							respuesta = Character.toLowerCase(teclado.next( ).charAt(0) );
+							}while(respuesta != 'i' && respuesta != 'c' && respuesta != 'd');
+										switch(respuesta){
+											
+										case 'i':
+										System.out.println("                           ______             ______        ");
+										System.out.println("                          #      #           #      #       ");
+										System.out.println("                         #        #         #        #      ");
+										System.out.println("                        #          #       #          #     ");
+										System.out.println("                       #      c     #     #     d      #    ");
+										System.out.println("                      #              #   #              #   ");
+										System.out.println("                     #                # #                #  ");
+												System.out.println("HAS PERDIDO, SE SIENTE");
+										break;
+										
+										
+										case 'c':
+											System.out.println("        ______                                ______        ");
+											System.out.println("       #      #                              #      #       ");
+											System.out.println("      #        #                            #        #      ");
+											System.out.println("     #          #            ###           #          #     ");
+											System.out.println("    #      i     #          #####         #     d      #    ");
+											System.out.println("   #              #         #####        #              #   ");
+											System.out.println("  #                #         ###        #                #  ");
+												System.out.println("ENHORABUENA, HAS GANADO");
+										break;
+										
+										case 'd':
+											System.out.println("        ______             ______       ");
+											System.out.println("       #      #           #      #      ");
+											System.out.println("      #        #         #        #     ");
+											System.out.println("     #          #       #          #    ");
+											System.out.println("    #      i     #     #      c     #   ");
+											System.out.println("   #              #   #              #  ");
+											System.out.println("  #                # #                # ");
+												System.out.println("HAS PERDIDO, SE SIENTE");
+										break;
+										}//Fin_switch
 				break;
 				
 				case 2:
-					System.out.println("La bola estara en la derecha");
+					//System.out.println("La bola estara en la derecha");
 					System.out.println("        ______             ______             ______        ");
-					System.out.println("       #      #           #      #           #      #          ");
-					System.out.println("      #        #         #        #         #        #         ");
-					System.out.println("     #          #       #          #       #          #        ");
-					System.out.println("    #            #     #            #     #            #       ");
-					System.out.println("   #              #   #              #   #              #      ");
-					System.out.println("  #                # #                # #                #     ");
+					System.out.println("       #      #           #      #           #      #       ");
+					System.out.println("      #        #         #        #         #        #      ");
+					System.out.println("     #          #       #          #       #          #     ");
+					System.out.println("    #      i     #     #      c     #     #     d      #    ");
+					System.out.println("   #              #   #              #   #              #   ");
+					System.out.println("  #                # #                # #                #  ");
 					System.out.println(" ");
-				break;
+					System.out.println("Escoja cubilete");
+						do{
+						System.out.println(" ");
+						System.out.println("i----> Izquierda");
+						System.out.println("c----> Centro");
+						System.out.println("d----> Derecha");
+								respuesta = Character.toLowerCase(teclado.next( ).charAt(0) );
+								}while(respuesta != 'i' && respuesta != 'c' && respuesta != 'd');
+										switch(respuesta){
+											
+										case 'i':
+										System.out.println("                           ______             ______        ");
+										System.out.println("                          #      #           #      #       ");
+										System.out.println("                         #        #         #        #      ");
+										System.out.println("                        #          #       #          #     ");
+										System.out.println("                       #      c     #     #     d      #    ");
+										System.out.println("                      #              #   #              #   ");
+										System.out.println("                     #                # #                #  ");
+												System.out.println("HAS PERDIDO, SE SIENTE");
+										break;
+										
+										
+										case 'c':
+											System.out.println("        ______                                ______        ");
+											System.out.println("       #      #                              #      #       ");
+											System.out.println("      #        #                            #        #      ");
+											System.out.println("     #          #                          #          #     ");
+											System.out.println("    #      i     #                        #     d      #    ");
+											System.out.println("   #              #                      #              #   ");
+											System.out.println("  #                #                    #                #  ");
+												System.out.println("HAS PERDIDO, SE SIENTE");
+										break;
+										
+										case 'd':
+											System.out.println("        ______             ______                      ");
+											System.out.println("       #      #           #      #                     ");
+											System.out.println("      #        #         #        #                    ");
+											System.out.println("     #          #       #          #         ###       ");
+											System.out.println("    #      i     #     #      c     #       #####      ");
+											System.out.println("   #              #   #              #      #####      ");
+											System.out.println("  #                # #                #      ###       ");
+												System.out.println("ENHORABUENA, HAS GANADO");
+										break;
+										}//Fin_switch
+				break;				
 			}//Fin_Switch
+			
 						
 		//Fin_Escoger cubilete y mostrar resultado
 		
